4f9d677c2bd66b41283840b250287946694b7738
VictorLeal44 <victorpaololeal@gmail.com>
2026-01-17 23:06:09 -0400

    manejo del control

--- ui/components.py
+++ ui/components.py
@@ -2,14 +2,23 @@ import flet as ft
 from core import *
 
 
+def estoy_aqui(e):
+    valor = e.control.data
+    if e.data == True:
+        print(f"Hiciste clic en: {valor}")
+        e.control.controls.clear()
+        e.control.controls.append(ft.ExpansionTile(title='>:v',controls = [],expand = True))
+        e.control.update()
+    else:
+        print('cerrado')
+
 def folders_groups(path_to_scan):
     folders = organizer.file_register(path_to_scan)['folders']
     groups = []
     try:
         for items in folders:
-            groups.append(ft.ExpansionTile(title=items,data = items))
+            groups.append(ft.ExpansionTile(title=items,data = items,controls = [],on_change=estoy_aqui))
         folders_home = ft.ExpansionTile(title='/home',controls = groups,expand = True)
-        print(items)
     except Exception as e:
         print(e)
     return folders_home
